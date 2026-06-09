33bac979b0552ba99bb7a80229a9d0a18cc9d871
Douglas Fantin <109866094+ImFantin@users.noreply.github.com>
2026-06-09 13:30:23 -0700

    Add: max-consecutive-ones - submission-1

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count = 0, res = 0;
+
+        for (int num : nums) {
+            if (num == 0) {
+                res = max(res, count);
+                count = 0;
+            } else {
+                count++;
+            }
+        }
+
+        return max(res, count);
+    }
+};
